9ad632a6d0c5b1412a19bee2d371c25cd62d6ca4
Nikunj Purohit <nikunj.p@simformsolutions.com>
2022-04-11 19:39:47 +0530

    changed database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_11_120505) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "category"
